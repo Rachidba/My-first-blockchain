0ffd47d7a94ac5fe1c1ed7fd9262d4aee35b6d2b
rachidba <rachidbaaziz.contact@gmail.com>
2018-07-20 10:44:28 +0100

    Add a helper class StringUtil

--- src/main/java/io/rachidba/util/StringUtil.java
+++ src/main/java/io/rachidba/util/StringUtil.java
@@ -1,4 +1,30 @@
 package io.rachidba.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class StringUtil {
+    // Applies Sha256 to a string and return the results.
+    public static String applySha256(String input) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest(input.getBytes("UTF-8"));
+            // This will contain hash as hexidecimal
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
