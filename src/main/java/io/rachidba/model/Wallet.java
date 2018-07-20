9f8e9c45797a1136dc65e879e77ca5ba32d96fa1
rachidba <rachidbaaziz.contact@gmail.com>
2018-07-20 20:23:54 +0100

    Add Wallet class
    	- For our rbcoin the public key will act as our address
    	- Our privat key is used to sign our transactions
    	- The generateKeyPair() methode use Elliptic-curve cryptography to
    	  generate our KeyPairs

---
+++ src/main/java/io/rachidba/model/Wallet.java
@@ -0,0 +1,33 @@
+package io.rachidba.model;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            // Initialize the key generator and generate a KeyPair
+            keyGen.initialize(ecSpec, random);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            // Set the public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
