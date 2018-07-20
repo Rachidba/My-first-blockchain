9f13bc782f5810da01e7ffe8e653a9c8ec1f24cb
rachidba <rachidbaaziz.contact@gmail.com>
2018-07-20 14:59:12 +0100

    Add mineBlock() methode to the Block class
    	The mineBlock() method takes in an int called difficulty, this is
    	the number of 0s they must solve for

--- src/main/java/io/rachidba/model/Block.java
+++ src/main/java/io/rachidba/model/Block.java
@@ -9,6 +9,7 @@ public class Block {
     public String previousHash;
     private String data; // Our data will be a simpme message
     private long timeStamp; // As number of milliseconds since 1/1/1970
+    private int nonce;
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -17,12 +18,22 @@ public class Block {
         this.hash = calculateHash();
     }
 
-    private String calculateHash() {
+    public String calculateHash() {
         String calculatedHash = StringUtil.applySha256(
                 this.previousHash +
-                        Long.toString(timeStamp) +
-                        data
-                    );
+                         Long.toString(timeStamp) +
+                         Integer.toString(nonce) +
+                         data
+                );
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(! this.hash.substring(0, difficulty).equals(target)) {
+            this.nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
